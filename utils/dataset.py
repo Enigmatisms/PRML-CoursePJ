92eb27b9cdef2993c04c3ace00854abba26bddbf
Enigmatisms <984041003@qq.com>
2022-11-17 21:55:10 +0800

    CustomDataset built

---
+++ utils/dataset.py
@@ -0,0 +1,70 @@
+#-*-coding:utf-8-*-
+"""
+    PRML-Proj 4: Custom dataset for pytorch loader: parallel worker loading
+    @author: Qianyue He
+    @date: 2022-11-17
+"""
+
+import os
+import torch
+import natsort
+import numpy as np
+from torch.utils import data
+        
+class CustomDataSet(data.Dataset):
+    """
+        root_dir: dataset root directory, should end with "/"
+    """
+    def __init__(self, root_dir: str, seq_num: int, transform = None, is_train = True, is_cuda = False, use_half = False):
+        self.is_train = is_train
+        self.root_dir = root_dir
+        train_prefix = "train" if is_train else "test"
+
+        self.data_dir = f"{root_dir}{train_prefix}_{seq_num}/"
+        self.label_dir = f"{root_dir}{train_prefix}_{seq_num}_label/"
+
+        self.transform = transform
+        img_names = filter(lambda x: x.endswith("dat"), os.listdir(self.data_dir))      # file name inside data_dir is the same with those in label_dir
+        all_ndarray = [name for name in img_names]
+        self.names = natsort.natsorted(all_ndarray)
+        
+        self.cuda = is_cuda
+        self.use_half = use_half
+        self.dev = "cuda" if is_cuda else "cpu"
+        self.float_type = {"np_float": np.float16, "torch_float": torch.float16} if use_half else {"np_float": np.float32, "torch_float": torch.float32}
+
+        if self.transform is None:
+            self.transform = lambda x: x
+
+    def __len__(self):
+        return len(self.names)
+
+    def __getitem__(self, idx):
+        data_loc = os.path.join(self.data_dir, self.names[idx])
+        label_loc = os.path.join(self.label_dir, self.names[idx])
+
+        data_value = np.fromfile(data_loc, dtype = np.uint8).astype(self.float_type["np_float"])        # all data are stored as u8, should be converted to float
+        tensor_data = torch.from_numpy(data_value).to(self.dev)
+        tensor_data = self.transform(tensor_data)   # preprocessing the loaded data (e.g. deterministic encoding)
+
+        raw_label = np.fromfile(label_loc, dtype = np.uint8).astype(self.float_type["np_float"])
+        tensor_label = torch.from_numpy(raw_label).to(self.dev)
+        return tensor_data, tensor_label
+
+    def debug_get(self, idx: int):
+        dats, lbls = self.__getitem__(idx)
+        print(dats.shape, dats.dtype, dats.device, lbls.shape, lbls.dtype, lbls.device)
+        
+    def __repr__(self):
+        return f"CustomDataSet(\n\tdata_dir={self.data_dir},\n" \
+            f"\tlabel_dir={self.label_dir},\n" \
+            f"\tuse_cuda={self.cuda}, use_half={self.use_half}, is_train={self.is_train}, len={len(self.names)}\n)"
+
+    def __str__(self):
+        return self.__repr__()
+
+if __name__ == "__main__":
+    print("Test dataset integrity.")
+    dataset = CustomDataSet("../data/", 500, use_half = True)
+    print(dataset)
+    dataset.debug_get(0)
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,24 @@
+#-*-coding:utf-8-*-
+"""
+    PRML-Proj 4: Utility functions
+    Generating easier-to-understand-and-load dataset from original .mtx and ATCG string txt
+    @author: Qianyue He
+    @date: 2022-11-16
+"""
+
 import numpy as np
 import argparse
 import tqdm
+import time
 from scipy.io import mmread
-
+from scipy.sparse import coo_matrix
 
 MAX_SEQ_LEN = 1500
 MAX_ENCODING_LEN = 4
 SQRT2 = 0.7071067811865476
 INPUT_PREFIX = "../data_project4/"
 INPUT_SUFFIX = "/sequences_"
+LABEL_SUFFIX = "/matrix_"
 
 MAPPING = {'A': 0, 'T': 1, 'C': 2, 'G': 3}
 ENCODING_BIG = np.array([
@@ -39,13 +49,24 @@ def convert_atcg_seqs(train: bool, out_prefix: str, seq_len: int):
             seqs[:length, :] = ENCODING[indices]
             seqs.tofile(f"{out_path_prefix}{i+1:05}.dat")
 
-def convert_labels(path: str, out_path: str, start_num: int = 1):
-    matrix = mmread(path)
-    print(matrix)
+def convert_labels(train: bool, out_prefix: str, seq_len: int):
+    folder_name = "train" if train else "test"
+    path = f"{INPUT_PREFIX}{folder_name}/{seq_len}{LABEL_SUFFIX}{folder_name}.mtx"
+
+    print(f"Start loading label matrix from '{path}'")
+    start_time = time.time()
+    matrix: coo_matrix = mmread(path)
+    result = matrix.toarray().astype(np.uint8)
+    del matrix
+    print(f"Conversion completed. Time consumption: {time.time() - start_time:.3f} s. Shape: {result.shape}")
+
+    for i, labels in enumerate(tqdm.tqdm(result)):
+        out_path_prefix = f"{out_prefix}{folder_name}_{seq_len}_label/seq_"
+        labels.tofile(f"{out_path_prefix}{i+1:05}.dat")
 
 # ==================== Mains ==========================
 def convert_atcg_main(args):
-    convert_atcg_seqs(args.is_train, args.atcg_out_prefix, args.atcg_seq_len)
+    convert_atcg_seqs(not args.is_test, args.atcg_out_prefix, args.atcg_seq_len)
 
 
 if __name__ == "__main__":
@@ -53,13 +74,11 @@ if __name__ == "__main__":
     # ================== ATCG converter args ==================
     parser.add_argument("--atcg_seq_len", type = int, default = 1000, help = "ATCG sequence length")
     parser.add_argument("--atcg_out_prefix", type = str, default = "../data/", help = "ATCG sequences folder path")
-    parser.add_argument("--is_train", default = True, action = "store_false", help = "Is train datasets?")
+    parser.add_argument("--is_test", default = False, action = "store_true", help = "Is test datasets?")
     # ================== Label matrix converter args ===============
-    parser.add_argument("--label_in_file", type = str, default = "../data_project4/train/500/sequences_train.txt", help = "ATCG sequences file path")
-    parser.add_argument("--label_out_path", type = str, default = "../data/train/", help = "ATCG sequences folder path")
-    parser.add_argument("--label_start_id", type = int, default = 1, help = "ATCG sequence numbering starting id")
+    parser.add_argument("--label_seq_id", type = int, default = 500, help = "ATCG sequence length")
+    parser.add_argument("--label_out_prefix", type = str, default = "../data/", help = "ATCG sequences folder path")
 
     args = parser.parse_args()
-
-    convert_atcg_main(args)
-    # convert_labels()
+    # convert_atcg_main(args)
+    convert_labels(not args.is_test, args.label_out_prefix, args.label_seq_id)
