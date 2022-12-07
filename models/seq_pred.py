75ee24a4860e7f6f7d4a888dde6d4589bd610f39
Enigmatisms <984041003@qq.com>
2022-12-07 14:49:47 +0800

    New backbone, trains successfully yet overfitted

--- models/seq_pred.py
+++ models/seq_pred.py
@@ -1,9 +1,6 @@
 #-*-coding:utf-8-*-
 """
-    Swin Transformer Layer implementation
-    This module is modified from my reproduction of Swin Transformer:
-    Github Maevit: https://github.com/Enigmatisms/Maevit
-    I am just reusing the Swin Transformer Layer as SwinTransformer1D
+    BN-Drop CNN Layer implementation
     @author: Qianyue He
     @date: 2022-11-19
 """
@@ -22,14 +19,15 @@ def makeConv1D(in_chan, out_chan, ksize = 3, act = nn.GELU(), norm = None, max_p
     return layer
 
 class PatchEmbeddings(nn.Module):
-    def __init__(self, ksize = 3, ksize_init = 3, out_channels = 128, input_channel = 4) -> None:
+    def __init__(self, ksize = 3, ksize_init = 3, out_channels = 128, input_channel = 4, input_drop = 0.1) -> None:
         super().__init__()
-        self.initial_mapping = nn.Linear(input_channel, out_channels >> 2, bias = False)
+        self.initial_mapping = nn.Linear(input_channel, out_channels >> 2)
         
         self.convs = nn.Sequential(
+            nn.Dropout(input_drop),
             *makeConv1D(out_channels >> 2, out_channels >> 1, ksize_init, norm = nn.BatchNorm1d(out_channels >> 1), padding = 0),
-            *makeConv1D(out_channels >> 1, out_channels, ksize, norm = nn.BatchNorm1d(out_channels), padding = 0),
-            *makeConv1D(out_channels, out_channels, ksize, norm = nn.BatchNorm1d(out_channels), padding = 0),
+            *makeConv1D(out_channels >> 1, out_channels, ksize, norm = nn.BatchNorm1d(out_channels),                padding = 0),
+            *makeConv1D(out_channels, out_channels, ksize, norm = nn.BatchNorm1d(out_channels),                     padding = 0, max_pool = 2),
         )
 
     def forward(self, X:torch.Tensor) -> torch.Tensor:
@@ -45,8 +43,6 @@ class SeqPredictor(nn.Module):
             nn.init.trunc_normal_(m.weight, std=.02)
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.Parameter):
-            nn.init.kaiming_normal_(m)
         elif isinstance(m, nn.Conv1d):
             nn.init.kaiming_normal_(m.weight)
             nn.init.constant_(m.bias, 0)
@@ -55,30 +51,30 @@ class SeqPredictor(nn.Module):
         self.emb_dim = emb_dim
         linear_dim = emb_dim << 2
         
-        self.patch_embed = PatchEmbeddings(3, 17, emb_dim, 4)
+        self.patch_embed = PatchEmbeddings(5, 17, emb_dim, 4, args.input_dropout)       # baseline (3, 17)
         self.avg_pool = nn.AdaptiveAvgPool1d(1)
         self.emb_drop = nn.Dropout(args.emb_dropout)
         self.conv_layer1 = nn.Sequential(
-            *makeConv1D(emb_dim,        emb_dim << 1,   3, norm = nn.BatchNorm1d(emb_dim << 1), max_pool = 2),
-            *makeConv1D(emb_dim << 1,   linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
+            *makeConv1D(emb_dim,        linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
+            *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
             *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
         )
         self.conv_layer2 = nn.Sequential(
             nn.Dropout(args.conv_dropout),
             *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
-            *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim),   max_pool = 2),
+            *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim)),
             *makeConv1D(linear_dim,     linear_dim,     3, norm = nn.BatchNorm1d(linear_dim)),
         )
 
         self.classify = nn.Sequential(
             nn.Dropout(args.class_dropout),
-            nn.Linear(linear_dim, linear_dim << 1),
+            nn.Linear(linear_dim, linear_dim >> 1),
+            nn.BatchNorm1d(1),
             nn.GELU(),
             nn.Dropout(args.class_dropout),
-            nn.Linear(linear_dim << 1, linear_dim),
-            nn.GELU(),
-            nn.Linear(linear_dim, 2000),
+            nn.Linear(linear_dim >> 1, 2000),
         )
+        # Baseline output: (linear_dim, 2000)
         # No sigmoid during classification (since there is one during AFL)
         self.apply(self.init_weight)
 
@@ -91,7 +87,7 @@ class SeqPredictor(nn.Module):
         self.load_state_dict(model_dict)
         if not opt is None:
             opt.load_state_dict(save['optimizer'])
-        print("Swin Transformer 1D loaded from '%s'"%(load_path))
+        print("BN-Drop CNN 1D loaded from '%s'"%(load_path))
         if not other_stuff is None:
             return [save[k] for k in other_stuff]
 
@@ -104,10 +100,38 @@ class SeqPredictor(nn.Module):
         X = self.avg_pool(X).transpose(-1, -2)      # shape after avg pooling: (N, 1, C)
         return self.classify(X).squeeze(dim = 1)    # output is of shape (N, 2000)
     
+    def get_wd_params(self, verbose = False):
+        decay = []
+        no_decay = []
+        whitelist_weight_modules = (nn.Linear, nn.Conv1d)
+        for mod_name, mod in self.named_modules():
+            if not isinstance(mod, whitelist_weight_modules):
+                if verbose:
+                    print(f"{type(mod)} Black listed, skipping...")
+                if isinstance(mod, nn.BatchNorm1d):
+                    for param in mod.parameters():
+                        no_decay.append(param)
+                continue
+            for param_name, param in mod.named_parameters():
+                if param_name.endswith('bias'):
+                    if verbose:
+                        print(f"{param_name} in {mod_name} is bias, excluded.")
+                    no_decay.append(param)
+                else:
+                    if verbose:
+                        print(f"{param_name} in {mod_name} are valid weights, added for weight decay.")
+                    decay.append(param)
+        return decay, no_decay
+           
 if __name__ == "__main__":
-    print("Swin Transformer 1D unit test")
+    print("BN-Drop CNN 1D unit test")
+    import sys
+    sys.path.append("..")
+    from utils.opt import get_opts
+    opt_args = get_opts()
     SEQ_LEN = 1000
-    stm = SeqPredictor(emb_dim = 96).cuda()
+    stm = SeqPredictor(opt_args, emb_dim = 96).cuda()
     test_seqs = torch.normal(0, 1, (8, 4, SEQ_LEN)).cuda()
     result = stm.forward(test_seqs)
+    stm.get_wd_params(verbose = True)
     
--- utils/opt.py
+++ utils/opt.py
@@ -11,13 +11,13 @@ def get_opts():
     # training parameters
     parser.add_argument("--seed", type = int, default = 0, help = "Random seed to use")
     parser.add_argument("--full_epochs", type = int, default = 1000, help = "Training lasts for . epochs (wo warmup and cooldown)")
-    parser.add_argument("--warmup_epochs", type = int, default = 0, help = "Warm up initialization epoch number")
+    parser.add_argument("--warmup_epochs", type = int, default = 10, help = "Warm up initialization epoch number")      # baseline 0
     parser.add_argument("--cooldown_epochs", type = int, default = 100, help = "Epochs for lr cooldown")
     parser.add_argument("--train_eval_time", type = int, default = 1, help = "Evaluate every <x> epoch(s) during training")
     parser.add_argument("--max_ckpt_num", type = int, default = 3, help = "Maximum number of checkpoint that can be stored")
     parser.add_argument("--train_verbose", type = int, default = 0, help = "Whether to output anything within an epoch (0 means no, x > 0 means output every x batches)")
     parser.add_argument("--mixup_epochs", type = int, default = 400, help = "Use mixup data augmentation for specified epochs")
-    parser.add_argument("--mixup", type = float, default = 0.3, help = "Whether to use mixup data augmentation")
+    parser.add_argument("--mixup", type = float, default = 0.4, help = "Whether to use mixup data augmentation")        # baseline 0.35
 
     # dataset & loader parameters
     parser.add_argument("--load_path", type = str, default = "", help = "Load path of model (or checkpoint), required when eval")
@@ -31,16 +31,17 @@ def get_opts():
 
     # model parameters
     parser.add_argument("--emb_dropout", type = float, default = 0.05, help = "Convolutional embedding dropout")
-    parser.add_argument("--class_dropout", type = float, default = 0.05, help = "Random path dropout rate")
-    parser.add_argument("--conv_dropout", type = float, default = 0.05, help = "Random path dropout rate")
+    parser.add_argument("--class_dropout", type = float, default = 0.05, help = "classification layer dropout rate")
+    parser.add_argument("--conv_dropout", type = float, default = 0.05, help = "Conv layer dropout rate")
+    parser.add_argument("--input_dropout", type = float, default = 0.05, help = "input layer (after mapping) dropout rate")
     parser.add_argument("--adam_wdecay", type = float, default = 3e-2, help = "Weight decay for AdamW")
 
     # asymmetrical loss parameters
     # TODO: defaults should be modified
     parser.add_argument("--asl_gamma_pos", type = float, default = 0.0, help = "asl gamma pos")
-    parser.add_argument("--asl_gamma_neg", type = float, default = 4.0, help = "asl gamma neg")
+    parser.add_argument("--asl_gamma_neg", type = float, default = 4.0, help = "asl gamma neg")     # baseline 5.0
     parser.add_argument("--asl_eps", type = float, default = 1e-8, help = "asl epsilon")
-    parser.add_argument("--asl_clip", type = float, default = 0.05, help = "asl clipping")
+    parser.add_argument("--asl_clip", type = float, default = 0.1, help = "asl clipping")           # baseline 0.05
 
     # lr scheduler parameters
     parser.add_argument("--cosine_folds", type = int, default = 5, help = "How many periods the cosine scheduler should have")
