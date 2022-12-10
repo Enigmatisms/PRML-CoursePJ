0c444b62b788c258c350e0ca5e02e168b84bf01a
Enigmatisms <984041003@qq.com>
2022-12-10 15:02:59 +0800

    MoCo result is not good, final visualization to be done.

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
+from models.simple_moco import MoCo, SimpleEncoder
 from models.seq_pred import SeqPredictor
-from sklearn.cluster import SpectralClustering
+from sklearn.cluster import SpectralClustering, KMeans
 from sklearn.metrics import normalized_mutual_info_score, adjusted_mutual_info_score
 
 LABEL_TABLE = { 'CLP': 0, 'CMP': 1, 'GMP': 2, 'HSC': 3, 'LMPP': 4, 
@@ -18,8 +19,43 @@ def get_gt_label(path: str):
             result.append(LABEL_TABLE[line])
         return np.array(result)
 
-def get_model_encoding(path: str):
+def get_model_embeddings(path: str, is_torch: bool = False):
     model = SeqPredictor()
     model.load(path)
+    # shape (2000, C)
+    weights = model.classify[-1].weight.detach()
+    if is_torch:
+        return weights
+    return weights.numpy()
+
+# Direct clustering without further post-processing
+def vanilla_spectrum_clutering(model_path: str, label_path: str):
+    gt_labels   = get_gt_label(label_path)
+    embeddings  = get_model_embeddings(model_path)
+    
+    sclt = SpectralClustering(n_clusters = 10, n_components = 10, gamma = 0.6, n_neighbors = 16, assign_labels = 'discretize', affinity = 'nearest_neighbors')
+    pred_labels = sclt.fit_predict(embeddings)
+    print(f"NMI: {normalized_mutual_info_score(gt_labels, pred_labels)}, AMI: {adjusted_mutual_info_score(gt_labels, pred_labels)}")
+    
+def moco_spectrum_clutering(model_path: str, label_path: str, moco_path: str):
+    gt_labels   = get_gt_label(label_path)
+    embeddings  = get_model_embeddings(model_path, True).cuda()
+    
+    moco_encoder = MoCo(SimpleEncoder).cuda()
+    moco_encoder.load(moco_path)
+    
+    reembedded = moco_encoder.base_encoder(embeddings).detach().cpu().numpy()
+    embeddings = embeddings.cpu().numpy()
+    sclt1 = SpectralClustering(n_clusters = 10, n_components = 10, gamma = 0.6, n_neighbors = 16, assign_labels = 'discretize', affinity = 'nearest_neighbors')
+    sclt2 = SpectralClustering(n_clusters = 10, n_components = 10, gamma = 0.6, n_neighbors = 16, assign_labels = 'discretize', affinity = 'nearest_neighbors')
+    moco_preds = sclt1.fit_predict(reembedded)
+    seqp_preds = sclt2.fit_predict(embeddings)
+    print(f"MOCO: NMI: {normalized_mutual_info_score(gt_labels, moco_preds)}, AMI: {adjusted_mutual_info_score(gt_labels, moco_preds)}")
+    print(f"Seq Predictor: NMI: {normalized_mutual_info_score(gt_labels, seqp_preds)}, AMI: {adjusted_mutual_info_score(gt_labels, seqp_preds)}")
+    
+if __name__ == "__main__":
+    moco_spectrum_clutering("./model/chkpt_2_res_bb_1000.pt", "./data_project4/celltype.txt", "./model/chkpt_2_moco_1000.pt")
+    
+    # vanilla_spectrum_clutering("./model/chkpt_2_res_bb_1000.pt", "./data_project4/celltype.txt")
 
 
--- utils/opt.py
+++ utils/opt.py
@@ -6,7 +6,8 @@
 
 import argparse
 
-def get_opts():
+# Options for Sequence predictor
+def get_predictor_opts():
     parser = argparse.ArgumentParser()
     # training parameters
     parser.add_argument("--exp_name", type = str, default = "baseline", help = "Name of experiment")
@@ -61,5 +62,51 @@ def get_opts():
     parser.add_argument("-d", "--del_dir", default = False, action = "store_true", help = "Delete dir ./logs and start new tensorboard records")
     parser.add_argument("-e", "--eval", default = False, action = "store_true", help = "Evaluation only model (a trained model is required)")
     parser.add_argument("-o", "--half_opt", default = False, action = "store_true", help = "Use AMP scaler to speed up")
+    parser.add_argument("--load_model", default = False, action = "store_true", help = "Load from model path or load from checkpoint path")
+    return parser.parse_args()
+
+# Options for Momentum Contrast
+def get_moco_opts():
+    parser = argparse.ArgumentParser()
+    # training parameters
+    parser.add_argument("--exp_name", type = str, default = "baseline", help = "Name of experiment")
+    parser.add_argument("--seq_model_dir", type = str, default = "./model/chkpt_2_", help = "Directory for seq predictor model")
+    parser.add_argument("--seq_model_name", type = str, default = "res_bb_", help = "Name for seq predictor model")
+
+    parser.add_argument('--moco_m', default=0.99, type=float, help='moco momentum of updating momentum encoder (default: 0.99)')
+    parser.add_argument("--seed", type = int, default = 0, help = "Random seed to use")
+    parser.add_argument("--full_epochs", type = int, default = 800, help = "Training lasts for . epochs (wo warmup and cooldown)")
+    parser.add_argument("--warmup_epochs", type = int, default = 100, help = "Warm up initialization epoch number")      # baseline 0
+    parser.add_argument("--cooldown_epochs", type = int, default = 600, help = "Epochs for lr cooldown")
+    parser.add_argument("--train_eval_time", type = int, default = 1, help = "Evaluate every <x> epoch(s) during training")
+    parser.add_argument("--max_ckpt_num", type = int, default = 3, help = "Maximum number of checkpoint that can be stored")
+    parser.add_argument("--train_verbose", type = int, default = 0, help = "Whether to output anything within an epoch (0 means no, x > 0 means output every x batches)")
+
+    # dataset & loader parameters
+    parser.add_argument("--load_path", type = str, default = "", help = "Load path of model (or checkpoint), required when eval")
+    parser.add_argument("--atcg_len", type = int, default = 1000, help = "Sequence number of a dataset")
+    parser.add_argument("--batch_size", type = int, default = 50, help = "Train set batch size")
+    parser.add_argument("--test_batch_size", type = int, default = 20, help = "Test set batch size")
+
+    parser.add_argument("--adam_wdecay", type = float, default = 3e-2, help = "Weight decay for AdamW")
+
+    # moco augmentation params
+    parser.add_argument("--max_masking_len", type = int, default = 8, help = "How many entries to mask (zeroing) for an embedding")
+    parser.add_argument("--std_scaler", type = float, default = 0.01, help = "Augmentation noise std (Gaussian)")
+    parser.add_argument("--masking_proba", type = float, default = 0.25, help = "Probability of masking (for the whole input)")
+
+    # lr scheduler parameters
+    parser.add_argument("--cosine_folds", type = int, default = 6, help = "How many periods the cosine scheduler should have")
+    parser.add_argument("--lr_max_start", type = float, default = 0.01, help = "The starting lr of upper bound lr")
+    parser.add_argument("--lr_max_end", type = float, default = 0.005, help = "The ending lr of upper bound lr")
+    parser.add_argument("--lr_min_start", type = float, default = 0.005, help = "The starting lr of lower bound lr")
+    parser.add_argument("--lr_min_end", type = float, default = 0.001, help = "The ending lr of lower bound lr")
+
+    # bool flags
+    parser.add_argument("-b", "--debug", default = False, action = "store_true", help = "Code debugging (detect gradient anomaly and NaNs)")
+    parser.add_argument("-d", "--del_dir", default = False, action = "store_true", help = "Delete dir ./logs and start new tensorboard records")
+    parser.add_argument("-e", "--eval", default = False, action = "store_true", help = "Evaluation only model (a trained model is required)")
+    parser.add_argument("-o", "--half_opt", default = False, action = "store_true", help = "Use AMP scaler to speed up")
+    parser.add_argument("-n", "--no_split", default = False, action = "store_true", help = "Train with full dataset with no train/test split")
     parser.add_argument("--load_model", default = False, action = "store_true", help = "Load from model path or load from checkpoint path")
     return parser.parse_args()
